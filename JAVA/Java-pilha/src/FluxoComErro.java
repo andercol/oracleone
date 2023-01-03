6c6beac62a516c76a938260d09bf75b7fd45c956
Anderson Colin <andercol@gmail.com>
2023-01-03 08:09:37 -0300

    checked e unchecked

--- JAVA/Java-pilha/src/FluxoComErro.java
+++ JAVA/Java-pilha/src/FluxoComErro.java
@@ -1,4 +1,4 @@
-public class Fluxo {
+public class FluxoComErro {
 
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
@@ -12,17 +12,15 @@ public class Fluxo {
 		System.out.println("Fim do main");
 	}
 
-	private static void metodo1() throws MinhaExcecao{
+	private static void metodo1() {
 		System.out.println("Ini do metodo1");
 		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 
-	private static void metodo2() throws MinhaExcecao {
-		System.out.println("Ini do metodo2");
-		
-		throw new MinhaExcecao("deu errado"); 
-		//throw ex;
-		//System.out.println("Fim do metodo2");
+	private static void metodo2() {
+		System.out.println("chamando metodo 2");
+		metodo2();
+		System.out.println("fim do metodo 2");
 	}
 }
